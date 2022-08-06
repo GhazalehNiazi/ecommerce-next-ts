8b20816775fc6049eef1b1b0aebe143f7bda1bc2
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2022-08-06 13:01:38 +0430

    fixed style of each product in the section

--- components/ShoppingItem.tsx
+++ components/ShoppingItem.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import Image from "next/image";
 import styles from "../styles/Shopping.module.css";
 import { Product } from "./types";
-import chair from '../public/chair.jpg'
+import chair from "../public/chair.jpg";
+import Link from "next/link";
 function ShoppingItem(props) {
   const {
     id,
@@ -18,18 +19,20 @@ function ShoppingItem(props) {
   }: Product = props.product;
 
   return (
-    <div>
-      <div className={styles.item}>
-        {img && <Image src={chair} width={250} height={250} />}
-      </div>
-      <div>
-        <h1>{name}</h1>
-        <h3>{genre}</h3>
-        <h3>{price}</h3>
-        <h3>{discount}</h3>
-        <h5>{available ? "available" : "not available"}</h5>
+    <Link href={`${id}`}>
+    <div className={styles.item}>
+      <div>{img && <Image src={chair} width={250} height={250} />}</div>
+      <div className={styles.itemDetail}>
+        <div className={styles.itemDetailFirstLine}>
+          <h1>{name}</h1>
+          <h3>{genre}</h3>
+        </div>
+        <h3>{price}$</h3>
+        {discount ? <h3>{discount} OFF</h3> : ""}
+        <h3>{available ? "available" : "not available"}</h3>
       </div>
     </div>
+    </Link>
   );
 }
 
